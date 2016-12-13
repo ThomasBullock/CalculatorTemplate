b310a1f2858011383ef39afa65599dc4188413bf
Thomas Bullock laptop <motbook@motbooks-MacBook-Pro.local>
2016-12-13 14:47:20 +1100

    Added results of pair programming Session Dec 13th

--- js/app.js
+++ js/app.js
@@ -1,4 +1,54 @@
-(function(){
-	console.log('Application is running!');
-})();
+// Get all the keys from document
+const keys = document.querySelectorAll('button');
+// console.log(keys);
+
+// Add onclick event to all the keys and perform operations
+keys.forEach(function(item) {
+  item.addEventListener('click', function(e) {
+    const keyPressed = e.target.value;
+  // console.log(keyPressed);
+	controller.input(keyPressed)
+  });
+});
+
+const controller = {
+  keyValue: [],
+  answer: 0,
+	displayOutput: answer,
+  input: function (keyValue) {
+    if (keyValue === '0' && controller.keyValue[0] === '0') {
+    } else if (keyValue === '=') {
+    	view.clear();
+      controller.calculate(controller.keyValue.join(''));
+			controller.displayOutput = controller.answer;
+			view.display(controller.displayOutput);
+
+		} else if(keyValue === 'c') {
+			view.clear();
+		}
+		else {
+      controller.keyValue.push(keyValue);
+			controller.displayOutput = controller.keyValue.join('');
+			view.display(controller.displayOutput);
+    }
+		// view.display(controller.keyValue.join(''));
+  },
+ calculate: function(str) {
+	  controller.answer = eval(str);
+		// console.log(controller.answer);
+ },
+};
+
+const view = {
+  display: function (str) {
+    let screen = document.getElementById('answer');
+    screen.innerHTML = str;
+    console.log(str);
+  },
+	clear: function () {
+  let screen = document.getElementById('answer');
+	screen.innerHTML = '';
+	}
+};
+
 //# sourceMappingURL=app.js.map
