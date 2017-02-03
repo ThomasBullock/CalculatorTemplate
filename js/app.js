0c8d43aee9835b53ee4dc6aaa77ab4fdcff108ba
Thomas Bullock <motbollox@gmail.com>
2017-02-03 17:37:09 +1100

    Added error handling, fixed minor bugs and improved styling

--- js/app.js
+++ js/app.js
@@ -6,18 +6,26 @@ const keys = document.querySelectorAll('button');
 keys.forEach(function(item) {
   item.addEventListener('click', function(e) {
     const keyPressed = e.target.value;
-  // console.log(keyPressed);
-	controller.input(keyPressed)
+  controller.input(keyPressed);
   });
 });
 
 const controller = {
+  operators: /[-\*+\/]/g,  // not currently being used
   keyValue: [],
   answer: 0,
-	displayOutput: this.answer,
+  displayOutput: this.answer,
   input: function (key) {
+    console.log(key);
+    if(key == "." && this.keyValue[this.keyValue.length-1] == ".") {  // prevent successive decimal points
+      return;
+    }
+    if(this.answer === 'error') {  // clear answer and keyValue arr if Error is onscreen
+      this.answer = 0;
+      this.keyValue = [];
+    }
     if (key === '0' && this.keyValue[0] === '0') {
-      this.keyValue[0] = '0';
+      return;
     } else if (key === '=') {
       this.calculate(this.keyValue.join(''));
       this.displayOutput = this.answer;
@@ -30,28 +38,34 @@ const controller = {
       this.displayOutput = this.keyValue.join('');
       view.display(this.displayOutput);
     }
-		// view.display(controller.keyValue.join(''));
   },
  calculate: function(str) {
-   this.answer = eval(str);
- // console.log(controller.answer);
+   try {
+    this.answer = eval(str);
+   } catch (e) {
+    console.log(e);
+      this.answer = 'error';
+   }
  },
 };
 
 const view = {
-
+  screen: document.getElementById('answer'), 
   display: function (str) {
-    const screen = document.getElementById('answer');
-    screen.innerHTML = str;
-    console.log(str);
+    if(str.length > 1 && str.charAt(0) === '0') { //to prevent 0 being added before other numbers if 0 is pressed first
+      this.screen.innerHTML = str.substring(1);      
+    } else {
+      this.screen.innerHTML = str;      
+    }
   },
 
-	clear: function () {
-  const screen = document.getElementById('answer');
-	screen.innerHTML = '0';
-  controller.answer = 0;
-  controller.keyValue = [];
-},
+  clear: function () {
+    this.screen.innerHTML = '0';
+    controller.answer = 0;
+    controller.keyValue = [];
+  },
 };
+
+//# sourceMappingURL=app.js.map
 
 //# sourceMappingURL=app.js.map
